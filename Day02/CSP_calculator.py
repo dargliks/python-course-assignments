4ddb4e8dc0bb741d5af1d9b2a719d4dc69f070a5
Dar Gliksberg <dargliks@gmail.com>
2026-04-25 23:17:08 +0300

    a little more commenting

--- Day02/CSP_calculator.py
+++ Day02/CSP_calculator.py
@@ -4,14 +4,17 @@
 
 #created by Dar Gliksberg, 25/04/26
 
+#input
 H1 = float(input("Spectrum 1 proton frequency (in ppm): "))
 N1 = float(input("Spectrum 1 nitrogen frequency (in ppm): "))
 H2 = float(input("Spectrum 2 proton frequency (in ppm): "))
 N2 = float(input("Spectrum 2 nitrogen frequency (in ppm): "))
 
+#calculations
 H_CSP = H1-H2
 N_CSP = N1-N2
 
 CSP_val = (((H_CSP **2) + ((N_CSP *0.2) **2)) **0.5)/2
 
+#print the output
 print ("Chemical Shift perturbation for this peak is: ", CSP_val)
