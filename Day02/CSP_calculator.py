b37153c57d695b15e081f0f90921db73042c0fe4
Dar Gliksberg <dargliks@gmail.com>
2026-04-25 23:15:38 +0300

    added some comments

--- Day02/CSP_calculator.py
+++ Day02/CSP_calculator.py
@@ -1,3 +1,9 @@
+#this is a simple program that calculates the chemical shift perturbations of a peak from two NMR spectra
+#currently designed for H-N 2D HSQC experiments (future goal: expand to further types of spectra)
+#currently requires manually entering the frequencies for a single peak (future goal: load frequencies from a file, to calculate multiple peaks simultaniously)
+
+#created by Dar Gliksberg, 25/04/26
+
 H1 = float(input("Spectrum 1 proton frequency (in ppm): "))
 N1 = float(input("Spectrum 1 nitrogen frequency (in ppm): "))
 H2 = float(input("Spectrum 2 proton frequency (in ppm): "))
