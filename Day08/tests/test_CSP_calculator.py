5a84bc3cac21b9fa890c2986bb3b1921f5662210
Dar Gliksberg <dargliks@gmail.com>
2026-06-07 11:44:12 +0300

    added more tests

--- Day08/tests/test_CSP_calculator.py
+++ Day08/tests/test_CSP_calculator.py
@@ -4,10 +4,11 @@
 
 import sys
 import os
+import pytest
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from CSP_calculator import determine_nuclei_types
+import CSP_calculator as cc
 
 
 # ==================================================
@@ -15,12 +16,12 @@ from CSP_calculator import determine_nuclei_types
 # ==================================================
 
 def test_determine_nuclei_types_hsqc():
-    result = determine_nuclei_types(1)
+    result = cc.determine_nuclei_types(1)
 
     assert result == ("1H", "15N", 1, 0.2)
 
 def test_determine_nuclei_types_hmqc():
-    result = determine_nuclei_types(2)
+    result = cc.determine_nuclei_types(2)
 
     assert result == ("13C", "1H", 1, 5)
 
@@ -29,3 +30,22 @@ def test_determine_nuclei_types_hmqc():
 # calc_CSP
 # ==================================================
 
+def test_calc_csp_hsqc_example():
+    result = cc.calc_CSP(
+        7.22, 7.556,       # 1H exp1/exp2
+        112.64, 117.593,   # 15N exp1/exp2
+        1,
+        0.2
+    )
+
+    assert result == pytest.approx(0.523, abs=0.001)
+
+def test_calc_csp_hmqc_example():
+    result = cc.calc_CSP(
+        20.328, 20.057,   # 13C exp1/exp2
+        0.793, 0.802,     # 1H exp1/exp2
+        1,
+        5
+    )
+
+    assert result == pytest.approx(0.275, abs=0.001)
